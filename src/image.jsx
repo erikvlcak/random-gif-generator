665ffd3a20d34e1db55ca22f8b8d149a7eb0c1d3
erikvlcak <erikvlcak1@gmail.com>
2024-01-06 21:51:03 +0100

    adjust layout of main components

--- src/image.jsx
+++ src/image.jsx
@@ -1,49 +1,13 @@
-import { useState } from 'react'
-import searchIcon from '../src/searchIcon.svg'
-
-function MemeImage() {
-  let [memeUrl, setMemeUrl] = useState('')
-  let [keyword, setKeyword] = useState('')
-
-  let changeKeyword = (e) => {
-    setKeyword(e.target.value)
-  }
-
-  let changeMeme = () => {
-    fetch(
-      `https://api.giphy.com/v1/gifs/random?api_key=rLV4XfJJoFueXHpX2lr3gMFzwlvnNgoK&tag=${keyword}&rating=g`,
-      { mode: 'cors' }
-    )
-      .then((fetchedData) => fetchedData.json())
-      .then((fetchedData) => setMemeUrl(fetchedData.data.images.original.url))
-  }
-
+/* eslint-disable react/prop-types */
+function MemeImage(props) {
   return (
-    <main className="flex flex-col justify-center items-center gap-4">
-      <div className="flex gap-4 justify-between">
-        <label htmlFor="search">Category:</label>
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Enter GIF category"
-          autoComplete="off"
-          className="border-orange-300"
-          onChange={changeKeyword}
-        />
-        <button type="button">
-          <img src={searchIcon} onClick={changeMeme} alt="search icon" />
-        </button>
-      </div>
-
-      <div>
-        {memeUrl ? <img src={memeUrl} alt="meme image" /> : <h1>Welcome!</h1>}
-      </div>
-
-      {/* <button type="button">
-        {memeUrl ? <p>Click to change</p> : <p>Click to generate</p>}
-      </button> */}
-    </main>
+    <div className="shadow-2xl">
+      {props.memeUrl ? (
+        <img src={props.memeUrl} alt="meme image" />
+      ) : (
+        <h1>No GIFs in this category.</h1>
+      )}
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './header'
-import MemeImage from './image'
+import Search from './search'
+import Footer from './footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <MemeImage />
+    <main className="h-screen flex flex-col justify-between">
+      <Header />
+      <Search />
+      <Footer />
+    </main>
   </React.StrictMode>
 )
