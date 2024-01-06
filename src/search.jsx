5fd3a67b2e0391df37c2891d08a584546c6bdb2d
erikvlcak <erikvlcak1@gmail.com>
2024-01-06 21:50:36 +0100

    create search component

---
+++ src/search.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import searchIcon from '../src/searchIcon.svg'
+import MemeImage from './image'
+
+function Search() {
+  let [memeUrl, setMemeUrl] = useState(
+    'https://media1.giphy.com/media/bcKmIWkUMCjVm/giphy.gif?cid=ecf05e473222cwc2pwi05eg0310kj7mia96wb7p1euaa29ln&ep=v1_gifs_search&rid=giphy.gif&ct=g'
+  )
+  let [keyword, setKeyword] = useState('random')
+
+  let changeKeyword = (e) => {
+    setKeyword(e.target.value)
+  }
+
+  let changeMeme = () => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=rLV4XfJJoFueXHpX2lr3gMFzwlvnNgoK&tag=${keyword}&rating=g`,
+      { mode: 'cors' }
+    )
+      .then((fetchedData) => fetchedData.json())
+      .then((fetchedData) => {
+        if (fetchedData.data.length != 0) {
+          setMemeUrl(fetchedData.data.images.original.url)
+        } else {
+          setMemeUrl('')
+        }
+      })
+  }
+
+  return (
+    <main className="flex  flex-col items-center gap-56 mb-auto">
+      <div className="flex gap-6 mt-20">
+        <label
+          htmlFor="search"
+          className="font-bold text-3xl flex items-center"
+        >
+          Category:
+        </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Random GIF"
+          autoComplete="off"
+          className="border-black border-b-2 focus:outline-none text-3xl"
+          onChange={changeKeyword}
+        />
+        <button
+          type="button"
+          className="bg-pink-300 p-4 rounded-xl drop-shadow-xl border-none border-0 active:bg-pink-500 transition-all"
+        >
+          <img src={searchIcon} onClick={changeMeme} alt="search icon" />
+        </button>
+      </div>
+      <MemeImage memeUrl={memeUrl} />
+    </main>
+  )
+}
+
+export default Search
